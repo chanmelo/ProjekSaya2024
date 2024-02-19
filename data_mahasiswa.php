fc6860c888a60c8f8bb6d12fd766d7c4eeb67651
chanmelo <159239434+chanmelo@users.noreply.github.com>
2024-02-19 15:25:51 +0700

    Update data_mahasiswa.php
    Update Data Semester

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1>Data Mahasiswa</h1>";
 echo "NIM : 22110049 <br>";
-echo "Nama Lengkap : Chantika Melolita";
-echo "Kelas : 22 MI B";
+echo "Nama Lengkap : Chantika Melolita<br>";
+echo "Kelas : 22 MI B<br>";
+echo "Semester Genap";
 ?>
